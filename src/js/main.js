0e0297ad1c59f164a305b52b92c8840868d10513
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-06-21 11:42:58 +0200

    Prepare front
    Add 3 li on ul (with the documents for the first folder)
    Add main.css

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
-import { Document } from './classes/Document';
-import { Folder } from './classes/Folder';
+import {Document} from './classes/Document';
+import {Folder} from './classes/Folder';
 
-let myFirstDocument = new Document("lolo","Florent", "Leseur", 1411460479000, 14007, "image/png");
-let mySecondDocument = new Document("Florent","Florent", "Leseur", 1411460479000, 14008, "image/png");
-let myThirdDocument = new Document("Rover","Florent", "Leseur", 1411460479000, 14009, "image/png");
+let myFirstDocument = new Document("lolo", "Florent", "Leseur", 1411460479000, 14007, "image/png");
+let mySecondDocument = new Document("Florent", "Florent", "Leseur", 1411460479000, 14008, "image/png");
+let myThirdDocument = new Document("Rover", "Florent", "Leseur", 1411460479000, 14009, "image/png");
 
 let myFirstFolder = new Folder("folder", "Florent", "Leseur", 12, [myFirstDocument, mySecondDocument, myThirdDocument]);
 
-console.log(myFirstFolder);
+let set = new Array();
+myFirstFolder.documents.forEach(function (doc) {
+    set.push(doc);
+})
+
+var d1 = document.getElementById('main');
+var html = '<ul>';
+set.forEach(function (elem) {
+    html += '<li>' + elem.logicalName + '</li>';
+});
+html += '</ul>';
+d1.insertAdjacentHTML('beforeend', html);
