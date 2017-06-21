ea8b7c2912c86e48061b03593454e7717d0a3e07
Boubou <jessy.baer@smartpanda.com>
2017-06-21 18:01:13 +0200

    Now we can draw a square and it works with scroll bar

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ let mka = document.getElementById("mka");
 
 if (!mka) throw new Error('mka id not found');
 
+mka.style.userSelect = "none";
+
 let zone = {
     downX: null,
     downY: null,
@@ -11,73 +13,104 @@ let zone = {
 };
 
 let selectItem = () => {
+    let mkaElts = document.getElementsByClassName("mka-elt");
+
+    let selectedItems = [];
+
+    // on parcours chaque elt pour savoir s'ils sont dans la zone selectionné 
+    Array.from(mkaElts).map(elt => {
+        console.log(elt.offsetTop);
+        let rect = elt.getBoundingClientRect();
+        // console.log(rect);
+        let zoneElt = {
+            x1: elt.offsetLeft,
+            x2: (elt.offsetLeft + rect.width),
+            y1: elt.offsetTop,
+            y2: (elt.offsetTop + rect.height)
+        }
+        // Permet de savoir si la zone de l'elt croise la zone de selection 
+        if (zone.x2 >= zoneElt.x1 && zoneElt.x2 >= zone.x1 && zone.y2 >= zoneElt.y1 && zoneElt.y2 >= zone.y1) {
+            elt.classList.add("mka-elt-selected");
+            selectedItems.push(elt);
+        } else {
+            elt.classList.remove("mka-elt-selected");
+
+        }
+    });
+
+    document.getElementById("mka-count").innerHTML = selectedItems.length;
+};
+
+// we add div
+let drawSquare = () => {
+    let node = document.createElement("div");
+    node.id = "selection";
+    mka.appendChild(node);
+    node.style.position = "absolute";
+    node.style.backgroundColor = "red";
+    return node;
+};
+
+let deleteSquare = () => {
+    let node = document.getElementById("selection");
+    mka.removeChild(node);
+}
+
+let orderCoordinate = () => {
     // we order coordinate to simplify
     zone.x1 = zone.downX;
     zone.x2 = zone.upX;
     zone.y1 = zone.downY;
     zone.y2 = zone.upY;
+    // on part vers la gauche
     if (zone.downX > zone.upX) {
         zone.x1 = zone.upX;
         zone.x2 = zone.downX;
     }
 
+    // on part vers le haut
     if (zone.downY > zone.upY) {
         zone.y1 = zone.upY;
         zone.y2 = zone.downY;
     }
+}
 
-    let mkaElts = document.getElementsByClassName("mka-elt");
+let refreshSquare = (node) => {
+    node.style.top = zone.y1 + "px";
+    node.style.left = zone.x1 + "px";
 
-    let selectedItems = [];
+    node.style.width = (zone.x2 - zone.x1) + "px";
+    node.style.height = (zone.y2 - zone.y1) + "px";
+}
 
-    Array.from(mkaElts).map(elt => {
-        let rect = elt.getBoundingClientRect();
-        let zoneElt = {
-            x1: rect.left,
-            x2: (rect.left + rect.width),
-            y1: rect.top,
-            y2: (rect.top + rect.height)
-        }
-
-        if (zone.x2 >= zoneElt.x1 && zoneElt.x2 >= zone.x1 && zone.y2 >= zoneElt.y1 && zoneElt.y2 >= zone.y1) {
-            elt.classList.add("mka-elt-selected");
-            selectedItems.push(elt);
-        }
-    });
-
-    document.getElementById("mka-count").innerHTML = selectedItems.length;
-
-};
-
+// Lors de la pression sur la souris on bind l'action de déplacement
 mka.onmousedown = (event) => {
-
-    let mkaElts = document.getElementsByClassName("mka-elt");
-    Array.from(mkaElts).map(elt => { elt.classList.remove("mka-elt-selected") });
-
+    // zone du click 
     zone.downX = event.pageX;
     zone.downY = event.pageY;
 
-    // we add div
-    let node = document.createElement("div");
-    node.id = "j";
-    node.style.position = "absolute";
-    node.style.top = zone.downY + "px";
-    node.style.left = zone.downX + "px";
-    node.style.backgroundColor = "red";
+    // on clean les éléments déjà sélectionné
+    let mkaElts = document.getElementsByClassName("mka-elt");
+    Array.from(mkaElts).map(elt => { elt.classList.remove("mka-elt-selected") });
 
-    mka.appendChild(node);
+    let node = drawSquare();
 
     document.body.onmousemove = (event) => {
         zone.upX = event.pageX;
         zone.upY = event.pageY;
-        node.style.width = (zone.upX - zone.downX) + "px";
-        node.style.height = (zone.upY - zone.downY) + "px";
+
+        orderCoordinate();
+        refreshSquare(node);
         selectItem();
     };
 };
 
+// lorsqu'on relache le clic
 window.onmouseup = () => {
+    // on unbind le mousemove
     document.body.onmousemove = () => { };
+    // on supprime la selection
+    deleteSquare();
 }
 
 
