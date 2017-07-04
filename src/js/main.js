78935abf55b274d511ee7cddf4aa290311f14524
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-04 16:46:36 +0200

    Rebrancher le click droit

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,7 @@ if (!mka) throw new Error('mka id not found');
 
 let config = {
     "focus": "mka",
+    "eltSelectableClass": "mka-elt",
     "eltSelectedClass": "mka-elt-selected",
     "onDragItemClass": null,
     "dragNdrop": true,
@@ -43,7 +44,7 @@ config.actions = [];
 //     config.actions[config.focus + '-arrow'](e);
 // }
 
-let selectables = [].slice.call(document.getElementsByClassName("mka-elt"));
+let selectables = [];
 let selection = [];
 let components = [];
 
@@ -51,6 +52,8 @@ HTMLElement.prototype.mkaInit = function (clientConfig) {
     Object.keys(clientConfig).map((i) => {
         config[i] = clientConfig[i];
     });
+
+    selectables = [].slice.call(document.getElementsByClassName(config.eltSelectableClass));
 
     // on désactive la selection de text
     mka.style.userSelect = "none";
@@ -96,6 +99,18 @@ HTMLElement.prototype.mkaInit = function (clientConfig) {
                 }
             }
             return false;
+        },
+        getSelectableElement: (elt) => {
+            if (elt.classList && elt.classList.contains(config.eltSelectableClass)) {
+                return elt;
+            }
+            while (elt.parentNode) {
+                elt = elt.parentNode;
+                if (elt.classList && elt.classList.contains(config.eltSelectableClass)) {
+                    return elt;
+                }
+            }
+            return null;
         },
         getSelectablesElements: () => {
             return selectables;
@@ -172,7 +187,7 @@ HTMLElement.prototype.mkaInit = function (clientConfig) {
         }
     }
 
-    let mouseEventsList = ["onmousedown", "onmousemove", "onmouseup", "ondblclick"];
+    let mouseEventsList = ["onmousedown", "onmousemove", "onmouseup", "onclick", "ondblclick"];
     let mouseEventsTargets = [
         {name: "windowEvents", value: window},
         {name: "documentEvents", value: document.body},
--- src/js/rightClick.js
+++ src/js/rightClick.js
@@ -1,73 +1,88 @@
-export let active = (mka, config) => {
-    config.actions["rc-arrow"] = onkeydown;
-    const mkarcmenuId = 'mkarcmenu'
+let parentFunctions = {};
+let config = {};
+const mkarcmenuId = 'mkarcmenu';
 
-    let removeMkaRcMenu = () => {
-        if (document.getElementById(mkarcmenuId)) {
-            document.getElementById(mkarcmenuId).remove();
-        }
+export let init = (conf, publicFunctions) => {
+    config = conf;
+    parentFunctions = publicFunctions;
+
+    bindContextMenu();
+}
+
+export let windowEvents = {
+    onkeydown: (event) => {
+
+    },
+    onclick: () => {
+        removeMkaRcMenu();
     }
+}
+
+let removeMkaRcMenu = () => {
+    if (document.getElementById(mkarcmenuId)) {
+        document.getElementById(mkarcmenuId).remove();
+    }
+}
+
+let bindContextMenu = () => {
 
     // On désactive le click droit sur l'élément principal
-    mka.addEventListener('contextmenu', (event) => {
+    parentFunctions.getContainer().addEventListener('contextmenu', (event) => {
         event.preventDefault();
 
-        // Si le menu est dèja open (au click droit sur la zone de sélection), on supprime le menu pour après en rajouter un nouveau
-        removeMkaRcMenu();
+        const selectableElement = parentFunctions.getSelectableElement(event.target);
+        if (selectableElement !== null) {
 
-        let mkaEltSelected = document.getElementsByClassName('mka-elt-selected')
-        // Si seulement 1 élément est sélectionné et que il y a des items pour le menu
-        if (mkaEltSelected.length === 1 && mkaEltSelected[0].hasAttribute('mka-rc-menu-items')) {
+            if (!parentFunctions.elementIsSelected(selectableElement)) {
+                parentFunctions.updateSelection([selectableElement]);
+            }
 
-            let newMenu = '<ul>';
+            let mkaEltSelected = parentFunctions.getSelection();
+            // Si seulement 1 élément est sélectionné et que il y a des items pour le menu
+            if (mkaEltSelected.length === 1 && mkaEltSelected[0].hasAttribute('mka-rc-menu-items')) {
 
-            let menuParse = mkaEltSelected[0].getAttribute('mka-rc-menu-items');
+                let newMenu = '<ul>';
 
-            let menu = JSON.parse(menuParse, function (key, value) {
-                if (value
-                    && typeof value === "string"
-                    && value.substr(0, 8) == "function") {
-                    var startBody = value.indexOf('{') + 1;
-                    var endBody = value.lastIndexOf('}');
-                    var startArgs = value.indexOf('(') + 1;
-                    var endArgs = value.indexOf(')');
+                let menuParse = mkaEltSelected[0].getAttribute('mka-rc-menu-items');
 
-                    return new Function(value.substring(startArgs, endArgs)
-                        , value.substring(startBody, endBody));
-                }
-                return value;
-            });
+                let menu = JSON.parse(menuParse, function (key, value) {
+                    if (value
+                        && typeof value === "string"
+                        && value.substr(0, 8) == "function") {
+                        var startBody = value.indexOf('{') + 1;
+                        var endBody = value.lastIndexOf('}');
+                        var startArgs = value.indexOf('(') + 1;
+                        var endArgs = value.indexOf(')');
 
-            menu.forEach(function (item) {
-                newMenu += `<li onclick="${item.action}; anonymous();">${item.title}</li>`;
-            });
+                        return new Function(value.substring(startArgs, endArgs)
+                            , value.substring(startBody, endBody));
+                    }
+                    return value;
+                });
 
-            newMenu += '</ul>';
+                Array.from(menu).map(item => {
+                    newMenu += `<li onclick="${item.action}; anonymous();">${item.title}</li>`;
+                });
 
-            const newDiv = document.createElement('div');
-            newDiv.setAttribute('id', mkarcmenuId);
-            document.body.appendChild(newDiv);
-            // Ajout au body d'une div contenant le menu du clic droit
-            newDiv.innerHTML = newMenu;
+                newMenu += '</ul>';
 
-            const mkarcmenu = document.getElementById(mkarcmenuId);
+                const newDiv = document.createElement('div');
+                newDiv.setAttribute('id', mkarcmenuId);
+                parentFunctions.getContainer().appendChild(newDiv);
+                // Ajout au body d'une div contenant le menu du clic droit
+                newDiv.innerHTML = newMenu;
 
-            mkarcmenu.style.position = 'absolute';
-            mkarcmenu.style.left = event.pageX + 'px';
-            mkarcmenu.style.top = event.pageY + 'px';
+                const mkarcmenu = document.getElementById(mkarcmenuId);
 
-            config.focus = "rc";
+                mkarcmenu.style.position = 'absolute';
+                mkarcmenu.style.left = event.pageX + 'px';
+                mkarcmenu.style.top = event.pageY + 'px';
 
+                config.focus = "rc";
+
+            }
         }
+
     });
 
-    window.onclick = () => {
-        removeMkaRcMenu();
-    };
 }
-
-let onkeydown = (e) => {
-    console.log(e);
-}
-
-
