9cf16a9b2460b146d7cd6ea6d5eac7719f84d793
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-10 15:07:23 +0200

    Multiinstance for copy paste

--- src/js/copyPaste.js
+++ src/js/copyPaste.js
@@ -1,18 +1,13 @@
-let parentFunctions = {};
-let config = {};
-let copyElts;
-
-export let init = (conf, publicFunctions) => {
-    config = conf;
-    parentFunctions = publicFunctions;
+export let init = (conf, parentFunctions) => {
+    parentFunctions.setProperty('copyElts', []);
 }
 
 export let windowEvents = {
-    onkeyup: (e) => {
-        if(e.which == '67' && e.ctrlKey) {
-            copyElts = parentFunctions.getSelection();
+    onkeyup: (e, parentFunctions, conf) => {
+        if (e.which == '67' && e.ctrlKey) {
+            parentFunctions.setProperty('copyElts', parentFunctions.getSelection());
         } else if (e.which == '86' && e.ctrlKey) {
-            config.pasteFunction(copyElts);
+            conf.pasteFunction(parentFunctions.getProperty('copyElts'));
         }
     }
 }
