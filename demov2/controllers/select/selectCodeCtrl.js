5ca42dcf0b18fe798b8d2d5aca01c52b8930f4f8
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-19 15:43:45 +0200

    Add code for select

---
+++ demov2/controllers/select/selectCodeCtrl.js
@@ -0,0 +1,22 @@
+app.controller('selectCodeCtrl', ['$scope', function ($scope) {
+
+    var vm = this;
+
+    vm.openCode = function (evt, codeName) {
+        var i, tabcontent, tablinks;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        document.getElementById(codeName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+
+}]);
