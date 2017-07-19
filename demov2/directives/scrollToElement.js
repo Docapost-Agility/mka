e5d1bdc8ca4f5866d06191a84a16c39aa30c0a22
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-19 17:58:11 +0200

    Add anchor scroll
    Add jquery dependancie
    Move id on home.html to container
    Add scrollToElement directive
    Change menu (removed sref and change by dirctive attributes)

---
+++ demov2/directives/scrollToElement.js
@@ -0,0 +1,14 @@
+app.directive('scrollToItem', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            scrollTo: "@"
+        },
+        link: function (scope, $elm, attr) {
+
+            $elm.on('click', function () {
+                $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top}, "slow");
+            });
+        }
+    }
+});
