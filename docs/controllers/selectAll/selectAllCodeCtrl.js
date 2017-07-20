b5ebd1b357b8cb3d92d0ead8ba2388db4baeb0c4
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-20 12:34:34 +0200

    Add code for selectAll

--- docs/controllers/selectAll/selectAllCodeCtrl.js
+++ docs/controllers/selectAll/selectAllCodeCtrl.js
@@ -1,7 +1,11 @@
-app.controller('selectAllCodeCtrl', ['$scope', function ($scope) {
+app.controller('selectAllCodeCtrl', [function () {
 
     var vm = this;
 
     vm.activeTab = 'HTML';
+
+    vm.codeCss = 'views/selectAll/selectAllCssCode.html';
+    vm.codeHtml = 'views/selectAll/selectAllHtmlCode.html';
+    vm.codeJs = 'views/selectAll/selectAllJsCode.html';
 
 }]);
