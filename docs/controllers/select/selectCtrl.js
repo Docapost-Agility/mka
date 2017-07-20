2b916828651d0eea92692bae78f8ff160fd922b3
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-20 11:45:06 +0200

    Add mkaActionsService and implement to select

--- docs/controllers/select/selectCtrl.js
+++ docs/controllers/select/selectCtrl.js
@@ -1,4 +1,4 @@
-app.controller('selectCtrl', ['$scope', function ($scope) {
+app.controller('selectCtrl', ['$scope', 'mkaActionsService', function ($scope, mkaService) {
 
     var vm = this;
 
@@ -6,87 +6,18 @@ app.controller('selectCtrl', ['$scope', function ($scope) {
     vm.mkaCountFolders = 'mka-count-folders-select';
     vm.mkaCountItems = 'mka-count-items-select';
 
-    var getContextMenuItems = function (selection) {
-        var menu = [];
-
-        menu.push({
-            title: "action 1",
-            action: function () {
-                alert("action 1");
-            }
-        });
-        menu.push({
-            title: "action 3",
-            action: function () {
-                alert("action 3");
-            }
-        });
-        if (selection.length > 1) {
-            menu.push({
-                title: "action multiple",
-                action: function () {
-                    alert("action multiple");
-                }
-            });
-        }
-
-        var htmlMenu = document.createElement('ul');
-
-        menu.forEach(function (item) {
-            var li = document.createElement('li');
-            li.innerHTML = item.title;
-            li.onclick = item.action;
-            htmlMenu.appendChild(li);
-        });
-
-        return htmlMenu;
-    }
-
-    var getContextMenuFolders = function (selection) {
-        var htmlMenu = '<ul>' +
-            '<li onclick="alert(\'action dossier 1\');">action dossier 1</li>' +
-            '<li onclick="alert(\'action dossier 3\');">action dossier 3</li>' +
-            '</ul>';
-
-        return htmlMenu;
-    }
-
-    var dbClick = function (elt) {
-        alert('dbclick ' + elt);
-    }
-
-    var drop = function (elts, zone) {
-        alert('drop ' + elts + ' dropZone ' + zone);
-    }
-
-    var deleteItems = function (elements) {
-        elements.forEach(function (elt) {
-            if (elt.parentNode) {
-                elt.parentNode.removeChild(elt);
-            }
-        })
-    }
-
-    var deleteFolders = function (elements) {
-        elements.forEach(function (elt) {
-            if (elt.parentNode && elt.parentNode.parentNode) {
-                elt.parentNode.parentNode.removeChild(elt.parentNode);
-            }
-        })
-    }
-
     $scope.$on('$includeContentLoaded', function () {
         document.getElementById('select').getElementsByClassName('itemsList').item(0).mkaInit({
             "eltsSelectable": "li",
             "onDragItemClass": "drag-class-sample",
-            "dragNdrop": drop,
+            "dragNdrop": mkaService.drop,
             "droppableElements": ".folders-container .folder",
-            "rightClick": getContextMenuItems,
-            "dbClick": dbClick,
+            "rightClick": mkaService.getContextMenuItems,
+            "dbClick": mkaService.dbClick,
             "lasso": true,
             "selectAllShortcut": true,
             "copyPaste": true,
-            "deleteShortcut": deleteItems,
+            "deleteShortcut": mkaService.deleteItems,
             "count": vm.mkaCountItems,
             "pasteFunction": function (items) {
                 console.log(items);
@@ -109,12 +40,12 @@ app.controller('selectCtrl', ['$scope', function ($scope) {
             "eltsSelectable": ".folder",
             "onDragItemClass": "drag-class-sample",
             "dragNdrop": true,
-            "rightClick": getContextMenuFolders,
-            "dbClick": dbClick,
+            "rightClick": mkaService.getContextMenuFolders,
+            "dbClick": mkaService.dbClick,
             "lasso": true,
             "selectAllShortcut": true,
             "copyPaste": true,
-            "deleteShortcut": deleteFolders,
+            "deleteShortcut": mkaService.deleteFolders,
             "count": vm.mkaCountFolders,
             "pasteFunction": function (items) {
                 console.log(items);
