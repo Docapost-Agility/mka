db96312f837f5001fd97a18c30bb048124594ff8
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-20 14:52:30 +0200

    Add code for rightClick

---
+++ docs/controllers/rightClick/rightClickCodeCtrl.js
@@ -0,0 +1,11 @@
+app.controller('rightClickCodeCtrl', [function () {
+
+    var vm = this;
+
+    vm.activeTab = 'HTML';
+
+    vm.codeCss = 'views/rightClick/rightClickCssCode.html';
+    vm.codeHtml = 'views/rightClick/rightClickHtmlCode.html';
+    vm.codeJs = 'views/rightClick/rightClickJsCode.html';
+
+}]);
