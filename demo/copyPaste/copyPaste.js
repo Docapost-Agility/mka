fa0430376291e9200baec41489634dccbda7fe27
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-24 17:04:36 +0200

    Remove old demo

--- demo/copyPaste/copyPaste.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("explication").innerHTML='<div class="panel panel-default">' +
-                                                    '<div class="panel-heading">COPY PASTE</div>' +
-                                                    '<div class="panel-body">To use copy paste, press Ctrl + C and Ctrl + V</div>' +
-                                                '</div>';
--- demo/rightClick/rightClick.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("explication").innerHTML='<div class="panel panel-default">' +
-                                                    '<div class="panel-heading">RIGHT CLICK</div>' +
-                                                    '<div class="panel-body">To use the right click, two options: leave the right click of the browser indicating no change in the mkaInit or activate to have a custom menu</div>' +
-                                                '</div>';
