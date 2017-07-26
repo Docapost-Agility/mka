6b81cd43198c722e03253f5d191631d74d08d1a1
Laurent Langlais <laurent.langlais@smartpanda.com>
2017-07-26 17:41:40 +0200

    Change prism by gist-embed
    Remove prism include and files
    Remove angularLoad
    Add gist-embed

--- docs/controllers/homeCtrl.js
+++ docs/controllers/homeCtrl.js
@@ -1,7 +1,3 @@
-app.controller('homeCtrl', ['$scope', 'angularLoad', function ($scope, angularLoad) {
-
-    $scope.$on('$includeContentLoaded', function () {
-        angularLoad.loadScript('js/prism.js');
-    });
+app.controller('homeCtrl', [function () {
 
 }]);
