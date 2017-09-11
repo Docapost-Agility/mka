71083989ae9f75356671ca8c3dba461e67c365ae
damien.fitte-duval <damien.fitte-duval@smartpanda.com>
2017-09-11 11:18:29 +0200

    Issue #18 Shift & arrows selection

--- src/js/main.js
+++ src/js/main.js
@@ -133,10 +133,10 @@ let getPublicFunctions = (container) => {
             return container.mkaParams.customProperties[key];
         },
         elementIsSelected: (elt) => {
-            if (elt.classList && elt.classList.contains(configs.eltSelectedClass)) {
+            if (elt && elt.classList && elt.classList.contains(configs.eltSelectedClass)) {
                 return true;
             }
-            while (elt.parentNode) {
+            while (elt && elt.parentNode) {
                 elt = elt.parentNode;
                 if (elt.classList && elt.classList.contains(configs.eltSelectedClass)) {
                     return true;
@@ -146,10 +146,10 @@ let getPublicFunctions = (container) => {
         },
         getSelectableElement: (elt) => {
             let selectables = container.mkaParams.selectables;
-            if (elt.classList && selectables.indexOf(elt) !== -1) {
+            if (elt && elt.classList && selectables.indexOf(elt) !== -1) {
                 return elt;
             }
-            while (elt.parentNode) {
+            while (elt && elt.parentNode) {
                 elt = elt.parentNode;
                 if (elt.classList && selectables.indexOf(elt) !== -1) {
                     return elt;
